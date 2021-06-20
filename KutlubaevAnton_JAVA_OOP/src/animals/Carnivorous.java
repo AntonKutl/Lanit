34ed72802e4575867a7d9706af4b664e2d9842ad
Антон <write_me@live.ru>
2021-06-20 17:14:01 +0300

    замечания устранены

--- KutlubaevAnton_JAVA_OOP/src/animals/Carnivorous.java
+++ KutlubaevAnton_JAVA_OOP/src/animals/Carnivorous.java
@@ -4,7 +4,6 @@ import foods.Food;
 import foods.Meat;
 
 public abstract class Carnivorous extends Animal {
-    public abstract String getName();
 
     @Override
     public String eat(Food food) {
